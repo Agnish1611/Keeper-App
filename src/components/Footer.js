e4d2cca2aa96ee0dc263a8844bd401d24eeab2f7
Agnish1611 <sukla.baruipur2@gmail.com>
2023-11-05 19:30:17 +0530

    Add Files

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+function Footer() {
+    return (
+        <footer>
+            <p>Copyright © {new Date().getFullYear()}</p>
+        </footer>
+    );
+}
+
+export default Footer;
